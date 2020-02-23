b3ea8792373b0a88cf64fa5986e9b8c9a5745e42
Ametov Pavel <pavelametov@gmail.com>
2020-02-23 19:31:51 +0300

    Update meduza

--- meduza.py
+++ meduza.py
@@ -4,8 +4,24 @@ from bs4 import BeautifulSoup
 baseUrl = 'https://meduza.io'
 response = requests.get(baseUrl)
 
-main_parser = BeautifulSoup(response.text, features = 'html.parser')
+main_parser = BeautifulSoup(response.text, features='html.parser')
 
 header_menu = main_parser.select_one('nav.Header-menu')
 categories = header_menu.select('a.Link-root')
-print(categories)
+for i in range(len(categories)):
+    category = categories[i]
+    text = category.text
+    print(f'{i + 1}] {text}')
+
+print('Select number of categories: ', end='')
+numberOfCategory = int(input())
+
+if numberOfCategory - 1 < 0 or numberOfCategory - 1 >= len(categories):
+    print('Incorrect number of category')
+    exit(-1)
+
+category = categories[numberOfCategory - 1]
+print(category.text)
+
+url = baseUrl + category['href']
+print(url)
