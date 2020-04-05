43301d4bea0abf0a50e9164211f74c1a990a214c
Ametov Artem <ametowartem@gmail.com>
2020-04-05 18:23:54 +0300

    add meduza

--- body.py
+++ body.py
@@ -8,9 +8,7 @@ def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
 
-@bot.message_handler(commands=['kek'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /kek')
+
 
 
 print('Started')
--- meduza-v2.py
+++ meduza-v2.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+from datetime import datetime
+import math
 
 baseUrl = 'https://meduza.io'
 response = requests.get(baseUrl)
@@ -23,11 +26,23 @@ if numberOfCategory - 1 < 0 or numberOfCategory - 1 >= len(categories):
 category = categories[numberOfCategory - 1]
 print(category.text)
 
-url = baseUrl + category['href']
-print(url)
+url = f'https://meduza.io/api/w5/search?chrono={category}&page=0&per_page=24&locale=ru'
 
 response = requests.get(url)
-second_parser = BeautifulSoup(response.text, features='html.parser')
 
-tmp = second_parser.find_all(class_= 'Skeleton-section')
-print('kek')
+if response.status_code != 200:
+    print('Error')
+    exit(-1)
+time1 = datetime.today()
+time = datetime.timestamp(time1)
+data = json.loads(response.text)
+documents = data.setdefault('documents')
+
+for key, value in documents.items():
+    #document = documents[i].copy
+    tmp = math.fabs(time - value['datetime'])
+    if tmp <= 86400:
+        title = value['title']
+        url_second = value['url']
+        print('https://meduza.io/' + url_second + ' ' + title)
+print(data)
